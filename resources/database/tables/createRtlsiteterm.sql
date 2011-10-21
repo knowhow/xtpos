c53b56acf2d3dac2c93dccc7a8b5dd854a493640
vsasa <sasa.vranic@bring.out.ba>
2011-10-21 18:28:11 +0200

    corecting the header licence info

--- resources/database/tables/createRtlsiteterm.sql
+++ resources/database/tables/createRtlsiteterm.sql
@@ -1,12 +1,27 @@
 --
--- This file is part of the knowhow ERP, a free and open source
--- Enterprise Resource Planning software suite,
--- Copyright (c) 2010-2011 by bring.out doo Sarajevo.
--- It is licensed to you under the Common Public Attribution License
--- version 1.0, the full text of which (including knowhow-specific Exhibits)
--- is available in the file LICENSE_CPAL_bring.out_knowhow.md located at the
--- root directory of this source code archive.
--- By using this software, you agree to be bound by its terms.
+-- The contents of this file are subject to the Common Public Attribution License Version 1.0 (the "License"); 
+-- you may not use this file except in compliance with the License. 
+-- You may obtain a copy of the License at http://www.xTuple.com/CPAL.  
+-- The License is based on the Mozilla Public License Version 1.1 but Sections 14 and 15 
+-- have been added to cover use of software over a computer network and provide for limited attribution 
+-- for the Original Developer. In addition, Exhibit A has been modified to be consistent with Exhibit B.
+--
+-- Software distributed under the License is distributed on an "AS IS" basis, 
+-- WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+-- See the License for the specific language governing rights and limitations under the License. 
+--
+-- The Original Code is xTuple ERP: PostBooks Edition
+--
+-- The Initial Developer of the Original Code is OpenMFG, LLC, d/b/a xTuple. 
+-- All portions of the code written by xTuple are Copyright (c) 1999-2011 
+-- OpenMFG, LLC, d/b/a xTuple. All Rights Reserved. 
+--
+-- Contributor(s):
+-- - hernad - Ernad Husremovic, hernad@bring.out.ba
+--
+-- ----------------------------------------------------------------------------
+-- 
+-- Modified by bring.out doo Sarajevo as part of knowhow ERP project 2010-2011.  
 --
 
 SELECT u2.execute($$
